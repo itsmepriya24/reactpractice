5c30a7c264b71165fb2e54d79b02d03fc3e84adc
Gurvinder <guruu.singh2033@gmail.com>
2022-05-12 18:19:32 +0530

    first commit

---
+++ src/Test.js
@@ -0,0 +1,12 @@
+import './App.css';
+import React from "react";
+
+function Test() {
+  return (
+    <div className="App">
+     <h1>Test</h1>
+    </div>
+  );
+}
+
+export default Test;
---
+++ src/User.js
@@ -0,0 +1,12 @@
+import './App.css';
+import React from "react";
+
+function User() {
+    return (
+      <div className="User">
+       <h1>React Users</h1>
+      </div>
+    );
+  }
+  
+  export default User;
