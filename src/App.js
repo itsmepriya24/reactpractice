44452b9b46cb19cb173fb6a7b91c3ba2e4673d03
Gurvinder <guruu.singh2033@gmail.com>
2022-06-01 14:56:23 +0530

    Assignment2

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Test2 from "./Test2.js";
 import Hide_show from "./Hide_show";
 import Form from "./Form";
 import Ifelse from "./Ifelse";
+import Assignment from "./Assignment";
 import Assignment2 from "./Assignment2";
 import User from "./User";
 import Parent from "./Parent";
@@ -17,6 +18,7 @@ function App() {
   return (
     <div className="App">
       <h1>React</h1>
+      {/* <Assignment /> */}
       <Assignment2 />
       {/* <Parent /> */}
       {/* <Test1 name="Onclick Event"/>
